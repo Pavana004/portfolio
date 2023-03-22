83c932c1f85fa516bdf72e260c5096e53f3f2f25
Pavan <101943293+Pavana004@users.noreply.github.com>
2023-03-22 13:12:13 +0530

    change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -28,7 +28,7 @@ const Nav = () => {
             <Link to='contact'smooth duration={500}>CONTACT</Link>
         </li>
         <li className='cursor-pointer hover:text-white'>
-            <a href="https://drive.google.com/file/d/1fji863FfkKQcUxOO1GXheea0PcBTycPA/view?usp=sharing" download>RESUME</a>
+            <a href="https://drive.google.com/file/d/1CLP6pxVHFJSfUQ_n6GbP7NAFoUAr5Ryy/view?usp=sharing" download>RESUME</a>
         </li>
       </ul>
        <div onClick={()=>setNav(!nav)} className='cursor-pointer pr-4 text-gray-400 md:hidden' >
@@ -52,7 +52,7 @@ const Nav = () => {
             <Link to='contact'smooth duration={500}>CONTACT</Link>
         </li>
         <li className='cursor-pointer hover:text-white'>
-            <a href="https://drive.google.com/file/d/1fji863FfkKQcUxOO1GXheea0PcBTycPA/view?usp=sharing" download>RESUME</a>
+            <a href="https://drive.google.com/file/d/1CLP6pxVHFJSfUQ_n6GbP7NAFoUAr5Ryy/view?usp=sharing" download>RESUME</a>
         </li>
        
        <FaTimes size={25} onClick={()=>setNav(false)} className="bg-gray-800 rounded-xl text-black"/>
